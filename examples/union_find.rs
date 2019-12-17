990725d1d4750ad806120a26aaa1c66bce88e22d
codercat <cat1067302838>
2019-12-17 15:43:13 +0800

    restructure

--- examples/union_find.rs
+++ examples/union_find.rs
@@ -1,23 +1,27 @@
 use algorithms::union_find::UnionFind;
-//use std::collections::HashMap;
 
 fn main() {
 
-    let mut union_find:UnionFind = UnionFind::new();
-    union_find.push(2);
-    union_find.push(21);
-    union_find.push(3);
-    union_find.push(1);
-    union_find.push(9);
-    union_find.union(3,1);
-    union_find.union(3,2);
-    union_find.union(3,21);
-    println!("{:?}", union_find.connected_component());
-//    let mut hm = HashMap::new();
-//    hm.insert(1, 2);
-//    hm.insert(11, 21);
-//    println!("{:?}", hm.contains_key(&111));
+    let mut union_find1: UnionFind = UnionFind::new();
+    union_find1.push(2);
+    union_find1.push(21);
+    union_find1.push(3);
+    assert_eq!(union_find1.connected_component(), 3);
+    assert_eq!(union_find1.is_connected(2, 3), false);
+    assert_eq!(union_find1.union(3,2), true);
+    assert_eq!(union_find1.union(3,2), false);
+    assert_eq!(union_find1.is_connected(2, 3), true);
+    assert_eq!(union_find1.connected_component(), 2);
 
+    let mut union_find2: UnionFind = UnionFind::from(vec![1, 2, 3, 4]);
+    assert_eq!(union_find2.connected_component(), 4);
+    union_find2.push(11);
+    assert_eq!(union_find2.connected_component(), 5);
+    assert_eq!(union_find2.is_connected(1, 2), false);
+    assert_eq!(union_find2.union(3,2), true);
+    assert_eq!(union_find2.union(3,2), false);
+    assert_eq!(union_find2.is_connected(2, 3), true);
+    assert_eq!(union_find2.connected_component(), 4);
 
 }
 
